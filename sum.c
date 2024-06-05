309eb2cf739979daab5d01d015c212acb61b4222
almamunsiraji <almamunsirajii@gmail.com>
2024-06-05 22:40:51 +0600

    fourth commit

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int n1, n2, sum=0;
+    printf(" Enter two integer: ");
+    scanf(" %d %d",&n1,&n2);
+    sum=n1+n2;
+    printf(" Sum= %d",sum);
+    return 0;
+}
